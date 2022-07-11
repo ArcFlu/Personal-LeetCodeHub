bb495589fe3522d6267e7d86c6438003272d50ff
Aldrich Agabin <93015179+ArcFlu@users.noreply.github.com>
2022-07-10 19:47:42 -0700

    Time: 128 ms (66.69%), Space: 13.9 MB (82.22%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.py
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -2,7 +2,7 @@ class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         rowList = [[0] * 9 for i in range(10)] 
         colList = [[0] * 9 for i in range(10)]
-        cellList = [[[0] * 9 for i in range(10)] for i in range(10)]
+        cellList = [[0] * 9 for i in range(10)]
         
         
         for i in range(9):
@@ -10,12 +10,12 @@ class Solution:
                 if board[i][j] == '.':
                     continue
                 currNum = int(board[i][j])
-                if rowList[i][currNum - 1] is 1 or colList[j][currNum - 1] is 1 or cellList[int(i/3)][int(j/3)][currNum - 1] is 1:
+                if rowList[i][currNum - 1] is 1 or colList[j][currNum - 1] is 1 or cellList[i // 3 * 3 + j // 3][currNum - 1] is 1:
                     return False
                 else:
                     rowList[i][currNum - 1] = 1
                     colList[j][currNum - 1] = 1
-                    cellList[int(i/3)][int(j/3)][currNum - 1] = 1
+                    cellList[i // 3 * 3 + j // 3][currNum - 1] = 1
 
     
         return True
