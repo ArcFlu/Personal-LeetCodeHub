e4bc2362621515264993ca44ed9697d4f5a9b329
Aldrich Agabin <93015179+ArcFlu@users.noreply.github.com>
2022-07-10 19:40:32 -0700

    Time: 167 ms (35.03%), Space: 13.8 MB (82.22%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.py
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -11,14 +11,6 @@ class Solution:
                     continue
                 currNum = int(board[i][j])
                 if rowList[i][currNum - 1] is 1 or colList[j][currNum - 1] is 1 or cellList[int(i/3)][int(j/3)][currNum - 1] is 1:
-                    print(rowList[i][currNum - 1])
-                    print(colList[j][currNum - 1])
-                    print(cellList[int(i/3)][int(j/3)][currNum - 1])
-                    print(rowList)
-                    print(colList)
-                    print(cellList)
-                    print(i)
-                    print(j)
                     return False
                 else:
                     rowList[i][currNum - 1] = 1
