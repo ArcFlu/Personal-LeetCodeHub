949ed3e8a4775d304444a0a1c3296d6436c07180
Aldrich Agabin <93015179+ArcFlu@users.noreply.github.com>
2022-07-30 15:43:47 -0700

    Time: 52 ms (93.66%), Space: 14.2 MB (52.93%) - LeetHub

--- 383-ransom-note/383-ransom-note.py
+++ 383-ransom-note/383-ransom-note.py
@@ -1,23 +1,3 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        if len(magazine) < len(ransomNote):
-            return False
-        
-        magDict = {}
-        
-        for i in magazine:
-            if i not in magDict:
-                magDict[i] = 1
-            else:
-                magDict[i] += 1
-                
-        for i in ransomNote:
-            if i not in magDict:
-                return False
-            else:
-                magDict[i] -= 1
-                if magDict[i] == 0:
-                    del magDict[i]
-            
-        
-        return True
+        return not collections.Counter(ransomNote) - collections.Counter(magazine)
